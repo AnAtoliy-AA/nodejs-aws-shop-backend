198d2b2f336be76a33e377beef180ed0bb0e425f
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-06-15 21:57:23 +0400

    feat: handle error scenarios

--- product-service/lambda-functions/getProductsById.ts
+++ product-service/lambda-functions/getProductsById.ts
@@ -3,6 +3,19 @@ import { products } from './products';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const productId = event.pathParameters?.productId;
+  
+  if (!productId) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: 'productId is required' }),
+    };
+  }
 
   const product = products.find(p => p.id === productId);
 
--- product-service/lambda-functions/getProductsList.ts
+++ product-service/lambda-functions/getProductsList.ts
@@ -1,7 +1,20 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { products } from "./products";
 
-export const handler: APIGatewayProxyHandler = async (event) => {
+export const handler: APIGatewayProxyHandler = async () => {
+  if (!products?.length) {
+    return {
+      statusCode: 404,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: 'NO Products found' }),
+    };
+  }
+
   return {
     statusCode: 200,
     headers: {
