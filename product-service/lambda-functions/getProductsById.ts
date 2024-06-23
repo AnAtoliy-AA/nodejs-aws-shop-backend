4620b5396ba0b0bd4a6880c4129db6a592ea5996
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-06-23 23:45:54 +0400

    feat: get product by id

--- product-service/lambda-functions/getProductsById.ts
+++ product-service/lambda-functions/getProductsById.ts
@@ -1,10 +1,69 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import { products } from './products';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import * as AWS from "aws-sdk";
+import { IProduct } from "./product.interface";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const PRODUCTS_TABLE_NAME = process.env.PRODUCTS_TABLE_NAME || "";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const productId = event.pathParameters?.productId;
-  
+
   if (!productId) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: "productId and title are required" }),
+    };
+  }
+
+  const params: DocumentClient.GetItemInput = {
+    TableName: PRODUCTS_TABLE_NAME,
+    Key: {
+      id: productId,
+    },
+  };
+
+  try {
+    
+    console.log(`Scanning table: ${PRODUCTS_TABLE_NAME}`);
+
+    const result = await dynamoDb.get(params).promise();
+
+    console.log("Scan result:", result);
+    
+    const product = result.Item as IProduct | undefined;
+
+    if (!product) {
+      return {
+        statusCode: 404,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: "Product not found" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    };
+  } catch (error: unknown) {
+    console.error("Error getting product:", error);
     return {
       statusCode: 500,
       headers: {
@@ -13,33 +72,10 @@ export const handler: APIGatewayProxyHandler = async (event) => {
         "Access-Control-Allow-Headers": "Content-Type",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ message: 'productId is required' }),
+      body: JSON.stringify({
+        message: "Error retrieving product",
+        error: (error as { message: string })?.message,
+      }),
     };
   }
-
-  const product = products.find(p => p.id === productId);
-
-  if (!product) {
-    return {
-      statusCode: 404,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ message: 'Product not found' }),
-    };
-  }
-
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(product),
-  };
 };
