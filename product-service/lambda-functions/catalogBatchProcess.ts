eac9a9beba95cbe7942f1363ac61c29f4bd1dcc1
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-07-04 21:27:04 +0400

    refactor: catalog batch process

--- product-service/lambda-functions/catalogBatchProcess.ts
+++ product-service/lambda-functions/catalogBatchProcess.ts
@@ -8,21 +8,22 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const PRODUCTS_TABLE_NAME = process.env.PRODUCTS_TABLE_NAME || "";
 const STOCKS_TABLE_NAME = process.env.STOCKS_TABLE_NAME || "";
 
-export const handler: SQSHandler = async (event: SQSEvent): Promise<void> => {
-  console.log("event:", event);
-
-  for (const record of event.Records) {
-    const { title, description, price, count } = JSON.parse(
-      record.body || "{}"
-    );
-
-    console.log('item:', record.body)
-
+const processRecord = ({
+  title,
+  description,
+  price,
+  count,
+}: {
+  title: string;
+  description: string;
+  price: number;
+  count: number;
+}) => {
+  return new Promise<void>(async (resolve, reject) => {
     const validationMessage = validateProduct({ title, price, count });
 
     if (validationMessage) {
-      console.log('validationMessage', validationMessage);
-
+      console.log("validationMessage:", validationMessage);
       return;
     }
 
@@ -52,10 +53,30 @@ export const handler: SQSHandler = async (event: SQSEvent): Promise<void> => {
         dynamoDb.put(stockParams).promise(),
       ]);
 
-      console.log("productsResult", productsResult);
-      console.log("stocksResult", stocksResult);
+      console.log("productsResult:", productsResult);
+      console.log("stocksResult:", stocksResult);
     } catch (error: unknown) {
       console.error("Error creating product:", error);
     }
+  });
+};
+
+export const handler: SQSHandler = async (event: SQSEvent): Promise<void> => {
+  console.log("event:", event);
+
+  const promises = event.Records.map(async (record) => {
+    console.log("item:", record.body);
+
+    const { title, description, price, count } = JSON.parse(
+      record.body || "{}"
+    );
+
+    return processRecord({ title, description, price, count });
+  });
+
+  try {
+    await Promise.all(promises);
+  } catch (error) {
+    console.error("Error processing one or more products:", error);
   }
 };
